fc58a9ef23950757e2191651247b3bd8b598b1eb
Jonathan Padilla <jpadilla8104@gmail.com>
2020-01-17 14:53:15 -0700

    changes to fizzbuzz.

--- src/edu/cnm/deepdive/FizzBuzz.java
+++ src/edu/cnm/deepdive/FizzBuzz.java
@@ -2,12 +2,12 @@ package edu.cnm.deepdive;
 
 public class FizzBuzz {
 
-  private static final int DEFAULT ROOF = 100; 
+  private static final int DEFAULT_ROOF = 100;
 
   public static void main(String[] args) {
 
-    int roof =
-    for(int i = 1; i <= roof; ++i) {
+    int roof = (args.length > 0) ? Integer.parseInt(args[0]) : DEFAULT_ROOF;
+    for (int i = 1; i <= roof; ++i) {
       System.out.println(toString(i));
     }
 
@@ -16,17 +16,18 @@ public class FizzBuzz {
 
   public static String toString(int num) {
     String result = "";
-        if (num % 15 == 0) {
-        result = "FizzBuzz";
-      } else if (num % 3 == 0) {
-        result = "Fizz";
-      } else if (num % 5 == 0)
-        ;
+    if (num % 15 == 0) {
+      result = "FizzBuzz";
+    } else if (num % 3 == 0) {
+      result = "Fizz";
+    } else if (num % 5 == 0) {
+
       result = "Buzz";
-    result = String.valueOf(num);
+    } else {
+      result = String.valueOf(num);
+    }
 
-
-      return "";
+    return result;
 
   }
 
